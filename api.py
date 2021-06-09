74a60122a4c147b9e88492fdd773a888888d9b8e
Nathan Moore <nathan-moore1@outlook.com>
2021-06-08 22:07:14 -0400

    update

--- api.py
+++ api.py
@@ -154,7 +154,7 @@ def jankModeActivated():
     htmlData = str(session.get('IFRAME_SOURCE'))
     linkTag = re.compile(r"<link.*>")
     scriptTag = re.compile(r"<script[^>]*>[^<]*</script>")
-    aTag = re.compile(r"<a[^>]*>[^<]*</a>")
+    aTag = re.compile(r"<a[^>]*>[^>]*</a>")
     stragglerTag = re.compile(r"<script[^>]*>[^>]*</script>")
     linkless = re.sub(linkTag, '', htmlData)
     scriptless = re.sub(scriptTag, '', linkless)
