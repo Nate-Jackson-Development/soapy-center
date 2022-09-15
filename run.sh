5ce490db614470288ed0f2ef1006a5003e2f4756
Nathan Moore <40650681+nate-moo@users.noreply.github.com>
2022-09-15 19:12:54 -0400

    mod run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
+#!/bin/sh
+
+cd /home/nathan/soapy-center/
 sudo gunicorn -t 60 --threads 2 -w 4 -b 0.0.0.0:443 --certfile=/var/local/SSL/naed3r.xyz.pem --keyfile=/var/local/SSL/naed3r.xyz.key api:app
